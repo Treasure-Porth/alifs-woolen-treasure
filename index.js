2935d6b76b5524c16ef9b46d676afbc37fd4a271
Treasure-Porth <112691973+Treasure-Porth@users.noreply.github.com>
2023-01-24 16:02:37 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+function buy(i) {
+    document.body.style.textTransform = "capitalize"
+    document.write("Click on ")
+    document.write("<br>")
+    let url = "https://www.facebook.com/messages/t/106688918474656/"
+    document.write("<a href='"+url+"' target='_blank' >Facebook</i></a>");
+    document.write(" or ")
+    let url1 = "https://www.instagram.com/direct/t/340282366841710300949128170586634597402"
+    document.write("<a href='"+url1+"' target='_blank'>Instagram </a>");
+    document.write("<br>")
+    document.write("To order ")
+    document.write("<br>")
+    document.write(" Write your address, number and the products code")
+    document.write("<br>")
+    document.write("which is " + i)
+    document.write("<br>")
+    document.write("We will reply in a few hours")
+}
+
+function one(){
+    document.getElementById("earring").src = "earring.jpg";
+}
+function two(){
+    document.getElementById("earring").src = "brownearring.jpg";
+}
+function three(){
+    document.getElementById("earring").src = "earring.jpg";
+}
+function bone(){
+    document.getElementById("bracelate").src = "bracelate.jpg";
+}
+function btwo(){
+    document.getElementById("bracelate").src = "bracelate2.jpg";
+}
+function tone(){
+    document.getElementById("tulip").src = "tulip.jpg";
+}
+function ttwo(){
+    document.getElementById("tulip").src = "tulip1.jpg";
+}
+function wone(){
+    document.getElementById("woolhat").src = "woolhat.jpg";
+}
+function wtwo(){
+    document.getElementById("woolhat").src = "wool2.jpg";
+}
+function bano(){
+    document.getElementById("bandana").src = "bandana1.jpg";
+}
+function bato(){
+    document.getElementById("bandana").src = "bandana.jpg";
+}
+function cone(){
+    document.getElementById("crochetscrunchie").src = "crochetscrunchie1.jpg";
+}
+function ctwo(){
+    document.getElementById("crochetscrunchie").src = "crochetscrunchie.jpg";
+}
+function kone(){
+    document.getElementById("keychain").src = "keychain.jpg";
+}
+function ktwo(){
+    document.getElementById("keychain").src = "keychain1.jpg";
+}
